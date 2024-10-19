251d1733c5e9d2be6fd4c263f3b32da9b3c1ba44
Rodrigo Liberti <liberti1991@gmail.com>
2024-10-19 20:10:06 -0300

    curso de vueJs

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header">dsf</header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+.header {
+  background: rgb(0, 0, 0);
+  color: white;
+  text-align: center;
+}
+</style>

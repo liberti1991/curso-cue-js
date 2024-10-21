2e51cb37b2501d27fcd91b5ad9fd5214dc85a6f6
Rodrigo Liberti <liberti1991@gmail.com>
2024-10-21 16:08:30 -0300

    todo project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
+import "@/assets/css/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from './router'
-import store from './store'
+import router from "./router";
+import store from "./store";
 
 window.app = createApp(App).use(store).use(router).mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="px-3 py-10 md:px-10">
+    <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
+      <TodoSpinner />
+
+      <TodoFormAdd />
+
+      <TodoItems />
+
+      <TodoEmpty />
+    </div>
   </div>
 </template>
 
 <script>
+import TodoEmpty from "@/components/TodoEmpty.vue";
+import TodoFormAdd from "@/components/TodoFormAdd.vue";
+import TodoItems from "@/components/TodoItems.vue";
+import TodoSpinner from "@/components/TodoSpinner.vue";
+import axios from "axios";
+
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: { TodoSpinner, TodoFormAdd, TodoItems, TodoEmpty },
+
+  data() {
+    return {
+      todos: [],
+    };
+  },
+
+  async created() {
+    this.todos = await axios
+      .get("http://localhost:3000/todos")
+      .then((response) => response.data)
+      .catch((error) => console.log(error));
+  },
+};
 </script>
